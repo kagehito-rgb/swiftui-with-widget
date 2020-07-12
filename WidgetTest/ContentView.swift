6cdf5cea8d39b5f3dac836902d9ae71f548f8942
kagehito-rgb <npu3tdz3mt@i.softbank.jp>
2020-07-12 13:43:48 +0900

    Initial Commit

---
+++ WidgetTest/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WidgetTest
+//
+//  Created by 中村裕紀 on 2020/07/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WidgetTest/WidgetTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetTestApp.swift
+//  WidgetTest
+//
+//  Created by 中村裕紀 on 2020/07/12.
+//
+
+import SwiftUI
+
+@main
+struct WidgetTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
